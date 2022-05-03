5c7cccc7147ac072b734892041bae9317fe2a37a
sekayasin <sekayasin@gmail.com>
2022-05-03 09:03:00 -0500

    - my portfolio

--- js/app.js
+++ js/app.js
@@ -1,33 +1,46 @@
+const sections = document.querySelectorAll('.section');
+const sectBtns = document.querySelectorAll('.controls');
+const sectBtn = document.querySelectorAll('.control');
+const allSections = document.querySelector('.main-content');
 
-var $overlay = $('<div id="overlay"></div>');
-var $image = $("<img>");
-var $caption = $("<p></p>");
 
-//An image to overlay
-$overlay.append($image);
+function PageTransitions() {
+    //Button click active class
+    for(let i = 0; i < sectBtn.length; i++) {
+        sectBtn[i].addEventListener('click', function(){
+            let currentBtn = document.querySelectorAll('.active-btn');
+            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
+            this.className += ' active-btn'
+        })
+    }
+    // sections Active
+    allSections.addEventListener('click', (e) =>{
+        const id = e.target.dataset.id;
+        if(id) {
+            // remove selected from the other btns
+            sectBtns.forEach((btn) => {
+                btn.classList.remove('active')
+            })
+            e.target.classList.add('active')
 
-//A caption to overlay
-$overlay.append($caption);
+            // hide other sections
+            sections.forEach((section) => {
+                section.classList.remove('active')
+            })
 
-//Add overlay
-$("body").append($overlay);
+            const element = document.getElementById(id);
+            element.classList.add('active')
+        }
+    })
 
-//capture the click event on a link to an image
-$("#gallery a").click( function(event){
-    event.preventDefault();
-    var imageLocation = $(this).attr("href");
-    $image.attr("src", imageLocation);
+    // light mode - Toggle theme
+    const themeBtn = document.querySelector('.theme-btn');
+    themeBtn.addEventListener('click', () => {
+        let element = document.body;
+        element.classList.toggle('light-mode');
+    })
 
-    //show the overlay
-    $overlay.show();
+}
 
-    //Get child's alt attribute and set caption
-    var captionText = $(this).children("img").attr("alt");
-    $caption.text(captionText);
-});
+PageTransitions();
 
-//when overlay is clicked
-$overlay.click(function(){
-    //Hide the overlay
-    $overlay.hide();
-});
